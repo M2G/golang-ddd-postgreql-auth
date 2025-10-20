63824ca4324e420d905c373275d3c6a150a90df9
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-10-20 02:41:52 +0200

    feat: refactor code, use gin-gonic instead of go-chi

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,23 +1,28 @@
 package router
 
-/*
 import (
 	"net/http"
 
-	"github.com/go-chi/chi"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-ddd-postgresql-auth/internal/handlers"
 	"github.com/sirupsen/logrus"
 )
 
-// A completely separate router for posts routes
-func PostRouter(pHandler *handlers.Post, log *logrus.Logger) http.Handler { // nolint
-	r := chi.NewRouter()
-	r.Get("/", pHandler.Fetch(log))
-	r.Get("/{id:[0-9]+}", pHandler.GetByID(log))
-	r.Post("/", pHandler.Create(log))
-	r.Put("/{id:[0-9]+}", pHandler.Update(log))
-	r.Delete("/{id:[0-9]+}", pHandler.Delete(log))
+func Routes(router *gin.Engine, pHandler *handlers.User, log *logrus.Logger) http.Handler {
+	log.Println("ok")
 
-	return r
+	router.GET("/ping", func(c *gin.Context) {
+		// Return JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/ok", pHandler.Fetch(log))
+	//r.Get("/{id:[0-9]+}", pHandler.GetByID(log))
+	//r.Post("/", pHandler.Create(log))
+	//r.Put("/{id:[0-9]+}", pHandler.Update(log))
+	//r.Delete("/{id:[0-9]+}", pHandler.Delete(log))
+
+	return router
 }
-*/
